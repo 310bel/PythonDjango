c632c5b13621bd4ba5ec707117cb2591f73b4186
Ilchenko Evgeniy <ilchenko_e@bsu.edu.ru>
2022-08-12 15:37:59 +0300

    Initial commit

--- taskmanager/main/urls.py
+++ taskmanager/main/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('',views.index, name='home'),
     path('about-us',views.about, name='about'),
     path('create',views.create, name='create'),
-path('', include('social_django.urls', namespace='social'))
+    path('', include('social_django.urls', namespace='social'))
 ]
